1422b1b8c773db464e504dbd829531b6003c2853
ysden123 <ysden123@gmail.com>
2017-03-26 21:35:12 +0300

    services

--- server/src/main/scala/com/stulsoft/scs/server/DbService.scala
+++ server/src/main/scala/com/stulsoft/scs/server/DbService.scala
@@ -1,34 +1,46 @@
 /*
  * Copyright (c) 2017. Yuriy Stul
  */
-
 package com.stulsoft.scs.server
 
 import com.stulsoft.scs.server.data.Data
+import com.typesafe.scalalogging.LazyLogging
 import slick.jdbc.H2Profile.api._
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
 /**
   * Database service
   *
   * @author Yuriy Stul
   */
-class DbService(val db: Database) {
+class DbService() extends LazyLogging {
 
   import scala.concurrent.ExecutionContext.Implicits.global
+
+  private val db = Database.forConfig("h2mem1")
+  private val setup = DBIO.seq(
+    dataTable.schema.create
+  )
+
+  logger.info("Database initialization started")
+  Await.result(db.run(setup), 2.seconds)
+  logger.info("Database initialization completed")
 
   def get(key: String): Future[Option[Data]] = Future {
     try {
       Integer.valueOf(key)
-//      Some(Data(key, "the value", 0L))
+      //      Some(Data(key, "the value", 0L))
       Option(Data(key, "the value", 0L))
     } catch {
-      case _ : Exception => None
+      case _: Exception => None
     }
   }
 
-  def put(key: String, json: String): Future[Option[Boolean]] = ???
+  def put(data: Data): Future[Option[Boolean]] = Future {
+    Option(true)
+  }
 
   def delete(key: String): Future[Option[Boolean]] = ???
 }
--- server/src/main/scala/com/stulsoft/scs/server/Service.scala
+++ server/src/main/scala/com/stulsoft/scs/server/Service.scala
@@ -6,30 +6,24 @@ package com.stulsoft.scs.server
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.{Directive, Directives}
-import com.stulsoft.scs.server.Server.logger
+import akka.http.scaladsl.server.{Directives, Route}
 import com.stulsoft.scs.server.data.Data
 import com.typesafe.scalalogging.LazyLogging
-import slick.jdbc.H2Profile.api._
 import spray.json._
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val dataFormat = jsonFormat3(Data)
+  implicit val dataFormat: RootJsonFormat[Data] = jsonFormat3(Data)
 }
 
 /**
   * @author Yuriy Stul
   */
 class Service extends Directives with JsonSupport with LazyLogging {
-  private val db = Database.forConfig("h2mem1")
-  private val dbService = new DbService(db)
-  private val version = "v.0.0"
-  init(db)
-
-  val route = pathPrefix(version) {
+  private lazy val dbService = new DbService()
+  val route: Route = pathPrefix("v.0.0") {
     pathPrefix("key" / Remaining) { key =>
       get {
         Await.result(dbService.get(key), 2.seconds) match {
@@ -40,21 +34,20 @@ class Service extends Directives with JsonSupport with LazyLogging {
         }
       } ~
         put {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Put for key $key</h1>"))
+          entity(as[Data]) {
+            data => {
+              Await.result(dbService.put(data), 2.seconds) match {
+                case Some(value) =>
+                  complete(value.toString)
+                case _ =>
+                  complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Failure</h1>"))
+              }
+            }
+          }
         } ~
         delete {
           complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Delete for key $key</h1>"))
         }
     }
-  }
-
-  private def init(db: Database): Unit = {
-    logger.info("Database initialization started")
-    val setup = DBIO.seq(
-      dataTable.schema.create
-    )
-
-    Await.result(db.run(setup), 2.seconds)
-    logger.info("Database initialization completed")
   }
 }
